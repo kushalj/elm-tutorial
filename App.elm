cf8f19217fc3a7185c1c0a4ed5767b474eee224c
Kushal <kushal@elysian.net>
2018-02-22 22:54:22 +0000

    Expand Collapse example

--- App.elm
+++ App.elm
@@ -1,56 +1,78 @@
 module App exposing (..)
 
-import Html exposing (Html, div, text, program)
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
 
 
 -- MODEL
 
+
 type alias Model =
-  String
+    Bool
+
 
 init : ( Model, Cmd Msg )
 init =
-  ( "Hello", Cmd.none )
+    ( False, Cmd.none )
 
 
 
 -- MESSAGES
 
+
 type Msg
-  = NoOp
+    = Expand
+    | Collapse
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ text model ]
+    if model then
+        div []
+            [ button [ onClick Collapse ] [ text "Collapse" ]
+            , text "Widget"
+            ]
+    else
+        div []
+            [ button [ onClick Expand ] [ text "Expand" ] ]
+
 
 
 -- UPDATE
 
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        Expand ->
+            ( True, Cmd.none )
+
+        Collapse ->
+            ( False, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
+
 -- MAIN
+
 
 main : Program Never Model Msg
 main =
     program
-      { init = init
-      , view = view
-      , update = update
-      , subscriptions = subscriptions
-      }
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
